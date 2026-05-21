3cfe35e7b842f86d1e352e5c41ce589f0b9dc587
Chris Legg <christopherlegg@me.com>
2026-05-21 12:19:34 +0000

    fix: correct base path and switch to HashRouter for GitHub Pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import "./index.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
